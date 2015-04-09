bb92b2a8a78d6504b7b8cf142302140730453428
Kevin Zhao <kevinzhaozifeng@gmail.com>
2015-04-09 13:13:55 -0500

    Add default parameter values

--- simBM/R/simBM.R
+++ simBM/R/simBM.R
@@ -6,7 +6,7 @@
 #' @export
 #' @return Vector of simulated BM path
 simBM<-
-  function(n, sigma){
+  function(n=1000, sigma=0.1){
     
     err=matrix(rnorm(2*n,0,sigma),ncol=2)
     x=apply(err,2,cumsum)
